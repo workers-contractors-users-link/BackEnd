5ae56af7bb80d357a3c15b738eab9a5fd053cfc4
Aditya <adityarpadekar@gmail.com>
2023-10-22 01:02:44 +0530

    bug fixes

--- controllers/client/signup.js
+++ controllers/client/signup.js
@@ -58,7 +58,7 @@ const signUpClient = async (req, res) => {
 
     // Send a response indicating successful sign up
     res.status(StatusCodes.OK).json({
-        msg: `Client, ${clientname} signed up`,
+        msg: `Client, ${client.name} signed up`,
         result: true,
     });
 };
